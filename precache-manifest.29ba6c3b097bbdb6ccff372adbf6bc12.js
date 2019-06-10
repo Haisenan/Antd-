2f14a592844934018be038a61173bbd18a1db3df
Haisenan <Haisenan@outlook.com>
2019-06-10 11:19:37 +0800

    提交文件

---
+++ precache-manifest.29ba6c3b097bbdb6ccff372adbf6bc12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa60e960d0603b0f0921e4636e30786",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b405ed412639127f8df0",
+    "url": "./static/css/2.62ddfbf0.chunk.css"
+  },
+  {
+    "revision": "4bc4fc0a17459ade4fc2",
+    "url": "./static/css/main.627e30c5.chunk.css"
+  },
+  {
+    "revision": "b405ed412639127f8df0",
+    "url": "./static/js/2.e57bed8a.chunk.js"
+  },
+  {
+    "revision": "4bc4fc0a17459ade4fc2",
+    "url": "./static/js/main.a257cb2a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "80bf6c84dd5f904a61304d0680e9c829",
+    "url": "./static/media/slider.80bf6c84.png"
+  },
+  {
+    "revision": "80bf6c84dd5f904a61304d0680e9c829",
+    "url": "./static/media/success.80bf6c84.png"
+  }
+]);
